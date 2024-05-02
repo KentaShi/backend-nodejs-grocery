7ad0cddef6c86887b8bc7e3f4adadba45d3fb87c
kentashi <kenta.nam.97@gmail.com>
2024-05-02 22:41:29 +0700

    optimize code

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,91 +8,9 @@ const {
 const { SuccessResponse } = require("../response/success.response")
 const ProductSerive = require("../services/product.service")
 
-const fetchAllProducts = async (req, res, next) => {
-    const { code, ...results } = await ProductSerive.findAll()
-    switch (code) {
-        case 200:
-            return new SuccessResponse({
-                message: "Found products",
-                metadata: results,
-            }).send(res)
-        case 404:
-            return new NotFoundResponse({ message: results?.message }).send(res)
-        default:
-            return new ErrorResponse({ message: results?.message }).send(res)
-    }
-}
-const fetchProductById = async (req, res, next) => {}
-const fetchProductByCategory = async (req, res, next) => {
-    const { cat } = req.params
-    const { code, ...results } = await ProductSerive.findByCategory(cat)
-    switch (code) {
-        case 200:
-            return new SuccessResponse({
-                message: `Found products by ${cat}`,
-                metadata: results,
-            }).send(res)
-        case 304:
-            return new NotModifiedResponse({ message: results?.message }).send(
-                res
-            )
-        default:
-            return new ErrorResponse({ message: results?.message }).send(res)
-    }
-}
-const addNewProduct = async (req, res, next) => {
-    const { code, ...results } = await ProductSerive.add(req.body)
-    switch (code) {
-        case 200:
-            return new SuccessResponse({
-                message: "Created product successfully",
-                metadata: results,
-            }).send(res)
-        case 304:
-            return new NotModifiedResponse({ message: results?.message }).send(
-                res
-            )
-        default:
-            return new ErrorResponse({ message: results?.message }).send(res)
-    }
-}
-const updateProductById = async (req, res, next) => {
-    const { id } = req.params
-    const data = req.body
-    const { code, ...results } = await ProductSerive.update(id, data)
-    switch (code) {
-        case 200:
-            return new SuccessResponse({
-                message: results?.message
-                    ? results.message
-                    : "Update product successfully",
-                metadata: results,
-            }).send(res)
-        case 404:
-            return new NotFoundResponse({ message: results?.message }).send(res)
-        default:
-            return new ErrorResponse({ message: results?.message }).send(res)
-    }
-}
-const deleteProductById = async (req, res, next) => {
-    const { id } = req.params
-    const { code, ...results } = await ProductSerive.delete({ product_id: id })
-    switch (code) {
-        case 200:
-            return new SuccessResponse({
-                message: "Delete product successfully",
-                metadata: results,
-            }).send(res)
-        case 404:
-            return new NotFoundResponse({ message: results?.message }).send(res)
-        default:
-            return new ErrorResponse({ message: results?.message }).send(res)
-    }
-}
-const searchProduct = async (req, res, next) => {
-    try {
-        const query = req.query.q
-        const { code, ...results } = await ProductSerive.searchProducts(query)
+class ProductController {
+    static fetchAllProducts = async (req, res, next) => {
+        const { code, ...results } = await ProductSerive.findAll()
         switch (code) {
             case 200:
                 return new SuccessResponse({
@@ -108,17 +26,113 @@ const searchProduct = async (req, res, next) => {
                     res
                 )
         }
-    } catch (error) {
-        next(error)
+    }
+    static fetchProductById = async (req, res, next) => {}
+    static fetchProductByCategory = async (req, res, next) => {
+        const { cat } = req.params
+        const { code, ...results } = await ProductSerive.findByCategory(cat)
+        switch (code) {
+            case 200:
+                return new SuccessResponse({
+                    message: `Found products by ${cat}`,
+                    metadata: results,
+                }).send(res)
+            case 304:
+                return new NotModifiedResponse({
+                    message: results?.message,
+                }).send(res)
+            default:
+                return new ErrorResponse({ message: results?.message }).send(
+                    res
+                )
+        }
+    }
+    static addNewProduct = async (req, res, next) => {
+        const { code, ...results } = await ProductSerive.add(req.body)
+        switch (code) {
+            case 200:
+                return new SuccessResponse({
+                    message: "Created product successfully",
+                    metadata: results,
+                }).send(res)
+            case 304:
+                return new NotModifiedResponse({
+                    message: results?.message,
+                }).send(res)
+            default:
+                return new ErrorResponse({ message: results?.message }).send(
+                    res
+                )
+        }
+    }
+    static updateProductById = async (req, res, next) => {
+        const { id } = req.params
+        const data = req.body
+        const { code, ...results } = await ProductSerive.update(id, data)
+        switch (code) {
+            case 200:
+                return new SuccessResponse({
+                    message: results?.message
+                        ? results.message
+                        : "Update product successfully",
+                    metadata: results,
+                }).send(res)
+            case 404:
+                return new NotFoundResponse({ message: results?.message }).send(
+                    res
+                )
+            default:
+                return new ErrorResponse({ message: results?.message }).send(
+                    res
+                )
+        }
+    }
+    static deleteProductById = async (req, res, next) => {
+        const { id } = req.params
+        const { code, ...results } = await ProductSerive.delete({
+            product_id: id,
+        })
+        switch (code) {
+            case 200:
+                return new SuccessResponse({
+                    message: "Delete product successfully",
+                    metadata: results,
+                }).send(res)
+            case 404:
+                return new NotFoundResponse({ message: results?.message }).send(
+                    res
+                )
+            default:
+                return new ErrorResponse({ message: results?.message }).send(
+                    res
+                )
+        }
+    }
+    static searchProduct = async (req, res, next) => {
+        try {
+            const query = req.query.q
+            const { code, ...results } = await ProductSerive.searchProducts(
+                query
+            )
+            switch (code) {
+                case 200:
+                    return new SuccessResponse({
+                        message: "Found products",
+                        metadata: results,
+                    }).send(res)
+                case 404:
+                    return new NotFoundResponse({
+                        message: results?.message,
+                    }).send(res)
+                default:
+                    return new ErrorResponse({
+                        message: results?.message,
+                    }).send(res)
+            }
+        } catch (error) {
+            next(error)
+        }
     }
 }
 
-module.exports = {
-    fetchAllProducts,
-    fetchProductById,
-    addNewProduct,
-    updateProductById,
-    deleteProductById,
-    searchProduct,
-    fetchProductByCategory,
-}
+module.exports = ProductController
