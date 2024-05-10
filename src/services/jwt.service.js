7737b585b9b91d1ef923ffff3c283c03462b431e
kentashi <kenta.nam.97@gmail.com>
2024-05-10 22:18:37 +0700

    use mongodb to check if refreshtoken exists instead of redis

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -5,65 +5,73 @@ const {
     AuthFailureError,
     UnauthorizedResponse,
 } = require("../response/error.response")
+const TokenService = require("./token.service")
 
-const signAccessToken = async (userId) => {
-    return new Promise((resolve, reject) => {
-        const payload = { userId }
-        const secret = process.env.ACCESS_TOKEN_SECRET
-        const options = {
-            expiresIn: "1m",
-        }
+class JWTService {
+    static signAccessToken = async (userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = { userId }
+            const secret = process.env.ACCESS_TOKEN_SECRET
+            const options = {
+                expiresIn: "1m",
+            }
 
-        JWT.sign(payload, secret, options, (err, res) => {
-            if (err) reject(err)
-            resolve(res)
+            JWT.sign(payload, secret, options, (err, res) => {
+                if (err) reject(err)
+                resolve(res)
+            })
         })
-    })
-}
+    }
 
-const signRefreshToken = async (userId) => {
-    return new Promise((resolve, reject) => {
-        const payload = { userId }
-        const secret = process.env.REFRESH_TOKEN_SECRET
-        const options = {
-            expiresIn: "1d",
-        }
+    static signRefreshToken = async (userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = { userId }
+            const secret = process.env.REFRESH_TOKEN_SECRET
+            const options = {
+                expiresIn: "1d",
+            }
 
-        JWT.sign(payload, secret, options, (err, token) => {
-            if (err) reject(err)
-            client.set(
-                userId.toString(),
-                token,
-                "EX",
-                365 * 24 * 60 * 60,
-                (err, reply) => {
-                    if (err) reject(err)
-                    resolve(token)
+            JWT.sign(payload, secret, options, async (err, token) => {
+                if (err) reject(err)
+                await TokenService.create({
+                    userId,
+                    refreshToken: token,
+                })
+                //resolve(token)
+                client.set(
+                    userId.toString(),
+                    token,
+                    "EX",
+                    365 * 24 * 60 * 60,
+                    (err, reply) => {
+                        if (err) reject(err)
+                        resolve(token)
+                    }
+                )
+            })
+        })
+    }
+
+    static verifyRefreshToken = async (refreshToken) => {
+        return new Promise((resolve, reject) => {
+            JWT.verify(
+                refreshToken,
+                process.env.REFRESH_TOKEN_SECRET,
+                (err, payload) => {
+                    if (err) return reject(err)
+                    client.get(payload.userId, (err, reply) => {
+                        if (err) return reject(err)
+                        if (refreshToken === reply) resolve(payload)
+                        return reject(
+                            new UnauthorizedResponse({
+                                message: "Invalid refresh token",
+                            })
+                        )
+                    })
                 }
             )
         })
-    })
+    }
 }
 
-const verifyRefreshToken = async (refreshToken) => {
-    return new Promise((resolve, reject) => {
-        JWT.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            (err, payload) => {
-                if (err) return reject(err)
-                client.get(payload.userId, (err, reply) => {
-                    if (err) return reject(err)
-                    if (refreshToken === reply) resolve(payload)
-                    return reject(
-                        new UnauthorizedResponse({
-                            message: "Invalid refresh token",
-                        })
-                    )
-                })
-            }
-        )
-    })
-}
-
-module.exports = { signAccessToken, signRefreshToken, verifyRefreshToken }
+module.exports = JWTService
