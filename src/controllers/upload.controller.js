2369de9da3f04cff85b0f88c761b8aea8a53650e
kentashi <kenta.nam.97@gmail.com>
2024-05-12 22:19:42 +0700

    upload thumb

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const {
+    BadRequestResponse,
+    ErrorResponse,
+} = require("../response/error.response")
+const { SuccessResponse } = require("../response/success.response")
+const UploadService = require("../services/upload.service")
+
+class UploadController {
+    uploadFileThumb = async (req, res, next) => {
+        try {
+            const { file } = req
+            if (!file) {
+                return new BadRequestResponse({ message: "File missing" })
+            }
+
+            const { code, ...results } =
+                await UploadService.uploadImageFromsLocal({ path: file.path })
+            if (code === 200) {
+                return new SuccessResponse({
+                    message: "Uploaded file successfully",
+                    metadata: results,
+                }).send(res)
+            }
+
+            return new ErrorResponse({ message: results?.message }).send(res)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+module.exports = new UploadController()
