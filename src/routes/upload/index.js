9aa28679351b0bbf226a1571465539117e5c20a4
kentashi <kenta.nam.97@gmail.com>
2024-05-16 22:40:42 +0700

    fix: Unexpected token 'n', nul  is not valid JSON

--- src/routes/upload/index.js
+++ src/routes/upload/index.js
@@ -1,12 +1,12 @@
 "use strict"
 const router = require("express").Router()
-const { uploadCloud } = require("../../config/multer.config")
+const { uploadDisk } = require("../../config/multer.config")
 const uploadController = require("../../controllers/upload.controller")
 const asyncHandler = require("../../helpers/asyncHandler")
 
 router.post(
     "/thumb",
-    uploadCloud.single("file"),
+    uploadDisk.single("file"),
     asyncHandler(uploadController.uploadFileThumb)
 )
 
